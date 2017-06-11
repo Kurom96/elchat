210a43cff7b95937db2b11076b8aa6b2fd9cf6b1
Kurom96 <kuroda2395@gmail.com>
2017-06-12 00:10:15 +0900

    done

---
+++ src/renderer/Avatar.jsx
@@ -0,0 +1,21 @@
+"use strict";
+
+import React from "react";
+
+const AVATAR_STYLE = {
+	width: 32,
+	textAlign: "center",
+	fontSize: 24
+};
+
+export default function Avatar(props) {
+	const { photoURL } = props.user;
+	if (photoURL) {
+		return <img className="img-rounded" src={photoURL} style={AVATAR_STYLE} />;
+	}
+	return (
+		<div style={AVATAR_STYLE}>
+			<span className="icon icon-user" />
+		</div>
+	);
+};
